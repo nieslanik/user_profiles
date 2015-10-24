9f98638ce527c92962c5694c57aec29849010143
Michael Simacek <msimacek@redhat.com>
2015-10-24 21:11:16 +0200

    Create Book entity
    Fixes #4

--- backend/src/main/java/cz/muni/fi/pa165/entity/Book.java
+++ backend/src/main/java/cz/muni/fi/pa165/entity/Book.java
@@ -1,8 +1,117 @@
 package cz.muni.fi.pa165.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Entity representing a single book in a library
+ *
+ * @author Michael Simacek
+ *
+ */
+@Entity
 public class Book {
 
-    public Book() {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String authorName;
+
+    @Column(nullable = false)
+    private Long isbn;
+
+    @Column(nullable = false)
+    private BookState state;
+
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public Long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Long isbn) {
+        this.isbn = isbn;
+    }
+
+    public BookState getState() {
+        return state;
+    }
+
+    public void setState(BookState state) {
+        this.state = state;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((authorName == null) ? 0 : authorName.hashCode());
+        result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Book)) {
+            return false;
+        }
+        Book other = (Book) obj;
+        if (authorName == null) {
+            if (other.authorName != null) {
+                return false;
+            }
+        } else if (!authorName.equals(other.authorName)) {
+            return false;
+        }
+        if (isbn == null) {
+            if (other.isbn != null) {
+                return false;
+            }
+        } else if (!isbn.equals(other.isbn)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
 }
