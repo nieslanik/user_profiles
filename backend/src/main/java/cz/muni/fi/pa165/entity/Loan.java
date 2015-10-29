407c457612b1ce2d8f51889405bdd1ad6c8889dc
xkubist <m.kubistel@gmail.com>
2015-10-29 11:12:46 +0100

    add @Entity annotation

--- backend/src/main/java/cz/muni/fi/pa165/entity/Loan.java
+++ backend/src/main/java/cz/muni/fi/pa165/entity/Loan.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
   * +[Loan]++-1>[Book]
   * @author xkubist
   */
+@Entity
 public class Loan {
     
     @Id
