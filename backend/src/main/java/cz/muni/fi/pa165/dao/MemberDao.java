f284858e8ab72654d3e3313f34afda67b0cca33c
Jakub Peschel <jakubpeschel@gmail.com>
2015-10-29 08:19:57 +0100

    [Fix] Added JavaDoc for MemberDao interface

--- backend/src/main/java/cz/muni/fi/pa165/dao/MemberDao.java
+++ backend/src/main/java/cz/muni/fi/pa165/dao/MemberDao.java
@@ -4,14 +4,46 @@ import java.util.List;
 
 import cz.muni.fi.pa165.entity.Member;
 
+/**
+ * Data manipulation operations for Member entity.
+ *
+ * @author Jakub Peschel
+ * @email jakub.peschel@studentagency.cz
+ */
 public interface MemberDao {
+    
+    /**
+     * Creates persistent representation of Member in the database. Sets the ID.
+     *
+     * @param member Member object to be persisted
+     * */
     void create(Member member);
 
+    /**
+     * Updates entity data in database
+     *
+     * @param member Member object to be updated
+     * */
     void update(Member member);
 
+    /**
+     * Finds a member in the database by its ID
+     *
+     * @param id
+     * @return Found Member object or null if no loan was found.
+     * */
     Member findById(Long id);
 
+    /**
+     * Retrieves all Member from the database
+     *
+     * @return List of all Members
+     * */
     List<Member> findAll();
 
+    /**
+     * Removes given Member from the database
+     * @param member
+     **/
     void delete(Member member);
 }
