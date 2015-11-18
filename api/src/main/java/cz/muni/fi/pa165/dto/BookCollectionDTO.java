c79042516a021a6a579f83693c22238d556f4390
xkubist <m.kubistel@gmail.com>
2015-11-18 14:23:45 +0100

    update BookCollectionDTO class

--- api/src/main/java/cz/muni/fi/pa165/dto/BookCollectionDTO.java
+++ api/src/main/java/cz/muni/fi/pa165/dto/BookCollectionDTO.java
@@ -1,5 +1,67 @@
 package cz.muni.fi.pa165.dto;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 public class BookCollectionDTO {
+    private Long id;
+    private String name;
+    private Set<BookDTO> books = new HashSet<>();
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<BookDTO> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<BookDTO> books) {
+        this.books = books;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        hash = 71 * hash + Objects.hashCode(this.name);
+        hash = 71 * hash + Objects.hashCode(this.books);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BookCollectionDTO other = (BookCollectionDTO) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.books, other.books)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
 }
