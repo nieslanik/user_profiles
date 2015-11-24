ebafc6a5fe78242e4f1791b307856e2edfad83a4
xkubist <m.kubistel@gmail.com>
2015-11-24 18:09:44 +0100

    fix bug in BookCollectionDao class

---
+++ api/src/main/java/cz/muni/fi/pa165/dto/CreateBookCollectionDTO.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.dto;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ *
+ * @author xkubist
+ */
+public class CreateBookCollectionDTO {
+
+    private String name;
+    private Set<BookDTO> books = new HashSet<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void addBook(BookDTO collection) {
+        books.add(collection);
+    }
+
+    public void setCollections(Set<BookDTO> books) {
+        this.books = books;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + Objects.hashCode(this.books);
+        return hash;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CreateBookCollectionDTO other = (CreateBookCollectionDTO) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.books, other.books)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
