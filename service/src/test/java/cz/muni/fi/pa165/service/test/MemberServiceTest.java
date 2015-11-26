3e2adf5af475f8816cd57a5c90fbbd2fc44ef581
xkubist <m.kubistel@gmail.com>
2015-11-26 14:54:44 +0100

    add MemberServiceTest class

--- service/src/test/java/cz/muni/fi/pa165/service/test/MemberServiceTest.java
+++ service/src/test/java/cz/muni/fi/pa165/service/test/MemberServiceTest.java
@@ -4,6 +4,8 @@
  */
 package cz.muni.fi.pa165.service.test;
 
+import cz.muni.fi.pa165.config.ServiceConfiguration;
+import cz.muni.fi.pa165.dao.MemberDao;
 import cz.muni.fi.pa165.entity.Book;
 import cz.muni.fi.pa165.entity.Loan;
 import cz.muni.fi.pa165.entity.Member;
@@ -13,21 +15,44 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 import javax.inject.Inject;
+import org.hibernate.service.spi.ServiceException;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author xkubist
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ServiceConfiguration.class)
 public class MemberServiceTest {
-    @Inject
-    MemberService service;
+     @Mock
+    private MemberDao productDao;
+    
+    
+
+    @Autowired
+    @InjectMocks
+    private MemberService service;
+
+    @BeforeClass
+    public void setup() throws ServiceException
+    {
+        MockitoAnnotations.initMocks(this);
+    }
     
     private Member member1;
     private Member member2;
@@ -52,35 +77,7 @@ public class MemberServiceTest {
         member2.setRegistrationDate(date);
         service.registerMember(member1, "totoJeNajneprelomitelnejsieHesloNaSvete");
         service.registerMember(member2, "OhFreddledGruntbugglyThyMicturationsAreToMe");
-        
-        Book book = new Book();
-        book.setName("Clean Code");
-        book.setIsbn(1L);
-        book.setState(BookState.NEW);
-        
-        loan1=new Loan();
-        loan1.setBook(book);
-        loan1.setDate(new Date(1));
-        loan1.setReturnBookState(BookState.NEW);
-        loan1.setReturnDate(new Date(3));
-        loan1.setReturned(Boolean.TRUE);
-        
-        loan2=new Loan();
-        loan2.setBook(new Book());
-        loan2.setDate(new Date(10));
-        loan2.setReturned(Boolean.FALSE);
-        
-        loan3=new Loan();
-        loan3.setBook(book);
-        loan3.setDate(new Date(5));
-        loan3.setReturnBookState(BookState.HEAVY_DAMAGE);
-        loan3.setReturnDate(new Date(8));
-        loan3.setReturned(Boolean.TRUE);
-        
-        service.addLoanToMember(member1, loan1);
-        service.addLoanToMember(member1, loan2);
-        service.addLoanToMember(member2, loan3);
-        
+
     }
     
     @Test
@@ -122,11 +119,6 @@ public class MemberServiceTest {
         service.deleteMember(myMember);
     }
     
-    @Test
-    public void testAddLoanToMember() {
-       Set<Loan> result = new HashSet<>(service.getAllLoans(member2));
-       assertSame(result.contains(loan3),Boolean.FALSE);
-    }
 
     @Test
     public void testGetAllLoansOfMember(Member member) {
