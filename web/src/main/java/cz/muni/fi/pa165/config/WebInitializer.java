855111f26d5d72a6129dafc258454c62ea771e4b
Michael Simacek <msimacek@redhat.com>
2015-12-09 13:47:05 +0100

    Add configuration for web
    Fixes #49

---
+++ web/src/main/java/cz/muni/fi/pa165/config/WebInitializer.java
@@ -0,0 +1,22 @@
+package cz.muni.fi.pa165.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { WebConfiguration.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
